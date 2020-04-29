a7f142b8ae4990e819ea38f6e2a7165695888b92
rutrut6969 <rutledgeisaac6969@gmail.com>
2020-04-28 21:49:33 -0500

    Giving up for tonight, got an error trying to call recipes.map

--- src/App/App.js
+++ src/App/App.js
@@ -1,20 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Nav from './Components/Nav/Nav';
 import Body from './Components/Body/Body';
-
-
+import axios from 'axios';
 
 function App() {
-  
-
+  useEffect(() => {
+    axios
+      .get('https://secretfamilyrecipes1.herokuapp.com/api/public')
+      .then((res) => console.log({ res }));
+  }, []);
   return (
     <div className='App'>
-    
-      
       <Nav />
       <Body />
     </div>
   );
-  }
+}
 
 export default App;
--- src/App/Components/Body/bodyComps/Forms/LoginForm.js
+++ src/App/Components/Body/bodyComps/Forms/LoginForm.js
@@ -17,29 +17,31 @@ const LoginForm = (props) => {
   };
 
   return (
-    <div className='signupForm'>
-    <form onSubmit={submitForm}>
-      <label htmlFor='Email' className='label email-label'></label>
-      <input
-        id='Email'
-        type='text'
-        name='Email'
-        onChange={handleChanges}
-        placeholder='Email'
-        value={box.email}
-      />
-
-      <label htmlFor='password' className='label pass-label'></label>
-      <input
-        id='password'
-        type='text'
-        name='password'
-        onChange={handleChanges}
-        placeholder='Password'
-        value={box.password}
-      />
-      <button type='submit'>Login</button>
-    </form>
+    <div className='login-form signupForm'>
+      <form onSubmit={submitForm}>
+        <h3>Login!</h3>
+        <label htmlFor='Email' className='label email-label'>
+          <input
+            id='Email'
+            type='text'
+            name='Email'
+            onChange={handleChanges}
+            placeholder='Email'
+            value={box.email}
+          />
+        </label>
+        <label htmlFor='password' className='label pass-label'>
+          <input
+            id='password'
+            type='text'
+            name='password'
+            onChange={handleChanges}
+            placeholder='Password'
+            value={box.password}
+          />
+        </label>
+        <button type='submit'>Login</button>
+      </form>
     </div>
   );
 };
