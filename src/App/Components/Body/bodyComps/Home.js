7fc68ca5b9dc78023d894a3ddf11300a3a3a5ed5
rutrut6969 <rutledgeisaac6969@gmail.com>
2020-04-28 19:10:30 -0500

    Started styling the home page and setting up the sk

--- src/App/Components/Body/bodyComps/Home.js
+++ src/App/Components/Body/bodyComps/Home.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import './home.css';
+import { useHistory } from 'react-router-dom';
 
 export default function Home(props) {
+  const { push } = useHistory();
+  const redirect = (e) => {
+    e.preventDefault();
+    push('/signup');
+  };
+
   return (
     <div className='home'>
-      <div>Hello From Home</div>
+      <section className='header'>
+        <div className='banner'>
+          Image goes here
+          <img src='' />
+        </div>
+        <div className='header-info'>
+          <h1>The Silent Cook</h1>
+          <h4>Store your families secret recipes with us!</h4>
+        </div>
+        <button className='signup' onClick={redirect}>
+          Signup Here!
+        </button>
+      </section>
     </div>
   );
 }
